cfc24c5a2487613eabd5b4b559fb90701c23045a
guardiangel <felix.sgq@gmail.com>
2024-03-12 13:18:55 -0400

    modify code

--- src/main/java/org/ac/cst8277/sun/guiquan/twitterclone/services/MessageService.java
+++ src/main/java/org/ac/cst8277/sun/guiquan/twitterclone/services/MessageService.java
@@ -6,19 +6,11 @@ import org.ac.cst8277.sun.guiquan.twitterclone.entities.MessageEntity;
 import org.ac.cst8277.sun.guiquan.twitterclone.repositories.MessageRepository;
 import org.ac.cst8277.sun.guiquan.twitterclone.utils.HttpResponseExtractor;
 import org.ac.cst8277.sun.guiquan.twitterclone.utils.UMSConnector;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
-import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.*;
 
 @Service("messageService")
@@ -27,11 +19,12 @@ public class MessageService {
     @Resource(name = "messageRepository")
     private MessageRepository messageRepository;
 
+    private String uri = "/getUserTokenByTokenId";
+
     @Resource(name = "umsConnector")
     private UMSConnector umsConnector;
 
     public boolean verifyToken(String token) {
-        String uri = "/getUserTokenByTokenId";
         Mono<Object> objectMono = umsConnector.retrieveUmsData(uri, token);
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         Future future = executorService.submit(() -> objectMono.block());
@@ -40,13 +33,11 @@ public class MessageService {
         try {
             tokenInfoMap = (Map<String, Object>) future.get();
             userTokenVo = HttpResponseExtractor.extractDataFromHttpClientResponse(tokenInfoMap, UserTokenVo.class);
-            System.err.println("userTokenVo=" + userTokenVo);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } catch (ExecutionException e) {
             throw new RuntimeException(e);
         }
-        System.err.println("tokenInfo=" + tokenInfoMap);
         executorService.shutdown();
         return userTokenVo != null;
     }
