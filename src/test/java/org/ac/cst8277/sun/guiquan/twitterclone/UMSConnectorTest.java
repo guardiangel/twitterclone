fc5c27b4a486a58aae9d2a5645dd37ce34f728ad
guardiangel <felix.sgq@gmail.com>
2024-03-25 17:57:27 -0400

    modify code

--- src/test/java/org/ac/cst8277/sun/guiquan/twitterclone/UMSConnectorTest.java
+++ src/test/java/org/ac/cst8277/sun/guiquan/twitterclone/UMSConnectorTest.java
@@ -1,24 +1,8 @@
 package org.ac.cst8277.sun.guiquan.twitterclone;
 
 import jakarta.annotation.Resource;
-import org.ac.cst8277.sun.guiquan.twitterclone.reponseVo.UserTokenVo;
-import org.ac.cst8277.sun.guiquan.twitterclone.utils.HttpResponseExtractor;
 import org.ac.cst8277.sun.guiquan.twitterclone.utils.UMSConnector;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.ExecutionException;
-
 @SpringBootTest
 public class UMSConnectorTest {
 
@@ -36,29 +20,29 @@ public class UMSConnectorTest {
      * ExecutorService executorService = Executors.newSingleThreadExecutor();
      * Future future = executorService.submit(() -> objectMono.block());
      */
-    @Test
-    public void test() {
-        String uri = "/getUserTokenByTokenId";
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJHdWlxdWFuIFN1biIsInJvbGVzIjpbIlJJQkVSIl0sImlhdCI6MTcxMTM5NTg4MywiZXhwIjoxNzExMzk2NzgzfQ.H3TsB29Z52gXAU4Fn84fUhAsY9XtRvYgLuWLadU-3HU";
-        WebClient client = WebClient.builder().baseUrl(uriUmsHost + ":" + uriUmsPort)
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .defaultHeader("token", token)
-//                .defaultHeader(HttpHeaders.AUTHORIZATION,
-//                        "Bearer " + token)
-                .build();
-        // define the request to send and its deserialization (in .bodyToMono(...))
-        Flux<Object> response = client.method(HttpMethod.GET).uri(uri).accept(MediaType.APPLICATION_JSON)
-                .acceptCharset(Charset.forName("UTF-8")).retrieve().bodyToFlux(Object.class);
-
-        try {
-            List<Object> list = response.collectList().toFuture().get();
-            list.forEach(o -> {
-                System.err.println(o);
-            });
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    @Test
+//    public void test() {
+//        String uri = "/getUserTokenByTokenId";
+//        String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJHdWlxdWFuIFN1biIsInJvbGVzIjpbIlJJQkVSIl0sImlhdCI6MTcxMTM5NTg4MywiZXhwIjoxNzExMzk2NzgzfQ.H3TsB29Z52gXAU4Fn84fUhAsY9XtRvYgLuWLadU-3HU";
+//        WebClient client = WebClient.builder().baseUrl(uriUmsHost + ":" + uriUmsPort)
+//                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+//                .defaultHeader("token", token)
+////                .defaultHeader(HttpHeaders.AUTHORIZATION,
+////                        "Bearer " + token)
+//                .build();
+//        // define the request to send and its deserialization (in .bodyToMono(...))
+//        Flux<Object> response = client.method(HttpMethod.GET).uri(uri).accept(MediaType.APPLICATION_JSON)
+//                .acceptCharset(Charset.forName("UTF-8")).retrieve().bodyToFlux(Object.class);
+//
+//        try {
+//            List<Object> list = response.collectList().toFuture().get();
+//            list.forEach(o -> {
+//                System.err.println(o);
+//            });
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        } catch (ExecutionException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 }
